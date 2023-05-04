ad443603c903ab0da1ac7c5dafda9e77c4e9997a
PurpxPanda <116929120+PurpxPanda@users.noreply.github.com>
2023-05-04 16:29:40 -0500

    fixed syntax error

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -2,12 +2,12 @@ const Intern = require("../lib/Intern");
 
 test("The Intern's school is  is created by getSchool()", () => {
     const school = "SAIT"
-    const employee = new Intern("John", 1, "ryan@test.com", school)
+    const employee = new Intern("", 1, "ryan@test.com", school)
     expect(employee.getSchool()).toBe(school)
 });
 
 test("Employee role of \"Intern\" is created by getRole()", () => {
     const role = "Intern"
-    const employee = new Intern("John", 1, "ryan@test.com", "SAIT")
+    const employee = new Intern("", 1, "ryan@test.com", "SAIT")
     expect(employee.getRole()).toBe(role)
 });
